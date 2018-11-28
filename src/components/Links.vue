5833c60ab817e2211923272092ef19edfcf7eb8e
RyanL <ryanl@RyanL.local>
2018-11-27 21:08:27 -0600

    Added links component with data

---
+++ src/components/Links.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="links">
+    <ul>
+      <li v-for="(link, key) in links" :key="key" class="link">
+        <a :href="link.url">{{ link.name }}</a>
+        <p v-for="(tag, key) in link.tags" :key="key">{{ tag }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Links',
+  data() {
+    return {
+      msg: 'hello there',
+      links: [{
+        'name': 'Ride Over Everything',
+        'url': 'https://rideovereverything.com/',
+        'tags': ['Front End', 'Back End'],
+        'type': 'website'
+      },{
+        'name': 'EZGO',
+        'url': 'https://ezgo.txtsv.com',
+        'tags': ['Front End', 'Back End'],
+        'type': 'website'
+      }],
+      skills: {
+        'web': ['Vue.js', 'Javascript', 'ES6', 'Drupal'],
+        'Animation': ['After Effects', 'Illustrator', 'Gifs', 'Videos']
+      }, 
+      videos: [{
+        'name': 'Mobile Assistant',
+        'url': '',
+        'tags': ['Animation', 'Sound Effects']
+      },{
+        'name': 'Mobile Assistant',
+        'url': '',
+        'tags': ['Animation', 'Sound Effects']
+      },{
+        'name': 'Building Regulation for Resilience Video | World Bank',
+        'url': '',
+        'tags': ['Character Animation']
+      }]
+    }
+  }
+}
+</script>
